58239871dfbf4ac88626862e3e2fc1ba2c76b36a
jplip <jpliporada@gmail.com>
2024-03-20 03:08:00 -0700

    no logistics

--- api/player.py
+++ api/player.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, jsonify
 from flask_restful import Api, Resource
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
 
 player_api = Blueprint('player_api', __name__,
@@ -27,7 +27,7 @@ X_scaled = scaler.fit_transform(X)
 X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
 
 # Train a logistic regression model
-model = LogisticRegression()
+model = LinearRegression()
 model.fit(X_train, y_train)
 
 # Function to predict the probability of depression
